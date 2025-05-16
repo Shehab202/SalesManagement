61a3a000129e3d518d902c2d1ec104e4b19c9ac7
shehab202 <shehabqoteit@gmail.com>
2025-05-16 17:48:48 +0300

    second commit

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,28 +1,25 @@
+
 import { initializeApp } from "firebase/app";
 import {
   getAuth,
   signInWithEmailAndPassword,
   createUserWithEmailAndPassword,
   GoogleAuthProvider,
-  // initializeAuth,
-  // getReactNativePersistence
 } from "firebase/auth";
-import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
+import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBoPopFYt97dt6UQttTQayPCvQaA8l-I9Q",
-  authDomain: "oldproject-20b10.firebaseapp.com",
-  projectId: "oldproject-20b10",
-  storageBucket: "oldproject-20b10.appspot.com",
-  messagingSenderId: "933869398038",
-  appId: "1:933869398038:web:36594eebdafcbf9cf1ed37",
+  apiKey: "AIzaSyC8WwaR2-PMsBbcMQjKhDnqsA4eh6X0YJM",
+  authDomain: "testproject-79c7a.firebaseapp.com",
+  projectId: "testproject-79c7a",
+  storageBucket: "testproject-79c7a.appspot.com",
+  messagingSenderId: "359801760728",
+  appId: "1:359801760728:web:7ac67174b207d1517ae0ad",
 };
-
 
 const app = initializeApp(firebaseConfig);
 
-// Auth, Firestore, Storage
 export const auth = getAuth(app);
 export const db = getFirestore(app);
 export const storage = getStorage(app);
@@ -36,25 +33,3 @@ export function signUp(email, password) {
   return createUserWithEmailAndPassword(auth, email, password);
 }
 
-/* 
-
-
-  import NetInfo from '@react-native-community/netinfo'
-  import firebase from 'firebase/compat/app'
-  import 'firebase/compat/auth'
-  import 'firebase/compat/firestore'
-  import 'firebase/compat/storage'
-  import ReactNativeAsyncStorage from '@react-native-async-storage/async-storage';
-
-  firebase.initializeApp(firebaseConfig)
-
-  export const app = initializeApp(firebaseConfig);
-
-  export const auth = initializeAuth(app, {
-    persistence: getReactNativePersistence(ReactNativeAsyncStorage)
-  });
-
-  export const db = initializeFirestore(app, {
-    experimentalForceLongPolling: true,
-  });
-*/
